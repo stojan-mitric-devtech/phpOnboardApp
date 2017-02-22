e4462223f248b82fdeef83025792931e3fce514e
stojan-mitric-devtech <stojan.mitric.contractor@ensighten.com>
2017-02-22 11:08:17 +0100

    Added psr-4 autoloader

---
+++ app/start.php
@@ -0,0 +1,8 @@
+<?php
+
+//session_start();
+//connect database
+
+//require_once 'models/User.php';
+
+require_once __DIR__ . '/../vendor/autoload.php';
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'app/start.php';
+
+//$user = new User();
+
+$topic = new Topic();
